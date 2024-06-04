23603177973e4fccb319de35d52986a3ba7cf1c5
Svitlana Sheptur <ssheptur@gmail.com>
2024-06-04 13:33:29 +0100

    migrate

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,9 +1,10 @@
 from drf_extra_fields.fields import Base64ImageField
-from recipes.models import (Favorite, Ingredient, IngredientRecipe, Recipe,
-                            ShoppingCart, Tag)
 from rest_framework import serializers
 from rest_framework.relations import SlugRelatedField
 from rest_framework.validators import UniqueTogetherValidator
+
+from recipes.models import (Favorite, Ingredient, IngredientRecipe, Recipe,
+                            ShoppingCart, Tag)
 from users.models import CustomUser, Subscription
 
 
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,4 +1,13 @@
 import django_filters
+from django.db.models import Sum
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
+from djoser.views import UserViewSet
+from rest_framework import status, viewsets
+from rest_framework.decorators import action
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.response import Response
+
 from api.filters import IngredientFilter, RecipeFilter
 from api.pagination import LimitPageNumberPagination
 from api.permissions import IsAuthorOrReadOnly
@@ -7,15 +16,7 @@ from api.serializers import (FavoriteCreateSerializer, IngredientSerializer,
                              RecipeReadSerializer,
                              ShoppingCartCreateSerializer,
                              SubscribeCreateSerializer, TagSerializer)
-from django.db.models import Sum
-from django.http import HttpResponse
-from django.shortcuts import get_object_or_404
-from djoser.views import UserViewSet
 from recipes.models import Favorite, Ingredient, Recipe, ShoppingCart, Tag
-from rest_framework import status, viewsets
-from rest_framework.decorators import action
-from rest_framework.permissions import AllowAny, IsAuthenticated
-from rest_framework.response import Response
 from users.models import Subscription
 
 
