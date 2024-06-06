8e559d81233a851400d35da6f609585af85de72d
Svitlana Sheptur <ssheptur@gmail.com>
2024-06-06 17:10:17 +0100

    abc

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,8 +1,7 @@
+from api.views import (IngredientViewSet, RecipeViewSet, TagViewSet,
+                       UserSubscriptionViewSet)
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-
-from api.views import (UserSubscriptionViewSet, IngredientViewSet,
-                       RecipeViewSet, TagViewSet)
 
 app_name = 'api'
 
